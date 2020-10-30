ca1a181950b8514d7e119957f7b4c876fa9813b2
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2020-10-30 19:36:53 -0300

    uploading all files

---
+++ src/cards.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Card = (props) => {
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card1-number card-i">
+            <i className="far fa-clock"></i>
+          </span>
+        </div>
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card1-number card-n">{props.one}</span>
+        </div>
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card2-number  card-n">{props.two}</span>
+        </div>
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card3-number  card-n">{props.three}</span>
+        </div>
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card4-number  card-n">{props.four}</span>
+        </div>
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card5-number  card-n">{props.five}</span>
+        </div>
+        <div className="card d-flex text-center justify-content-center">
+          <span className="card6-number  card-n">{props.six}</span>
+        </div>
+      </div>
+      <div className="row mt-5 ml-1 justify-content-center">
+        <button className="btn-c " id="btn-pause" onClick={props.pause}>
+          Pause
+        </button>
+        <button className="btn-c" id="btn-reset" onClick={props.reset}>
+          Reset
+        </button>
+        <button className="btn-c" id="btn-resume" onClick={props.resume}>
+          Resume
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  one: PropTypes.number,
+  two: PropTypes.number,
+  three: PropTypes.number,
+  four: PropTypes.number,
+  five: PropTypes.number,
+  six: PropTypes.number,
+  pause: PropTypes.func,
+  reset: PropTypes.func,
+  resume: PropTypes.func,
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap';
+import './App.css';
+import Card from './cards'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+let count = [0,0,0,0,0,0,0];
+let ak = 9
+let intervalID;
+let flag = true;
+
+let pauseButton = () => {
+  clearInterval(intervalID);
+  intervalID = null;
+}
+
+
+let resumeButton = () => {
+  if (flag) {
+intervalID = setInterval(timer,1000)
+  }
+}
+
+let resetButton = () => {
+  clearInterval(intervalID);
+  count = [0,0,0,0,0,0,0];
+  intervalID = setInterval(timer, 1000);
+}
+
+
+
+
+const timer = function(){
+
+setTimeout(function(){
+  if (count[6] === ak) {
+    count[6] = 0;
+    if (count[5] === ak) {
+        count[5] = 0
+        if (count[4] === ak) {
+            count[4] = 0
+            if (count[3] === ak) {
+                count[3] = 0
+                if(count[2] === ak) {
+                    count[2] = 0
+                    if (count[1] === ak) {
+                        if (count[0] === ak) {
+                            count = [0,0,0,0,0,0,0];    
+                        }
+                    } else {
+                        count[1]++
+                    }
+                } else {
+                    count[2]++
+                }
+            } else {
+                count[3]++
+            }
+        } else {
+            count[4]++
+        }
+    } else {
+        count[5]++
+    }
+  }  else {
+  count[6]++;
+  }
+}, 500)
+
+
+ 
+ 
+ ReactDOM.render(
+   <Card six={count[6]} five={count[5]} four={count[4]} three={count[3]} two={count[2]} one={count[1]} reset={resetButton} pause={pauseButton} resume={resumeButton}/>,
   document.getElementById('root')
-);
+ );
+ 
+ }
+
+ intervalID = setInterval(timer, 1000)
+
+// interval={pauseTimer()}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
